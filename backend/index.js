bece3ccdbb98a9d63fff62b97b5a53a1d2f1af02
elipmoc <relipmoc101@gmail.com>
2019-05-01 08:11:11 +0900

    サーバーでソケットで配信するの実装

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const io = require('socket.io')();
+io.on('connection', clientSocket => { 
+    console.log("connected!");
+    clientSocket.on("send",data=>{
+        console.log("send:");
+        console.log(data);
+        io.emit(data.event,data.data);
+    });
+ });
+io.listen(3000);
+console.log("listen 3000 port")
