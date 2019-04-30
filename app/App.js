0d16011fb18d74885546ed855299ae9bd7edaace
44x1carbon <b4079@oic.jp>
2019-05-01 00:48:50 +0900

    ボタン押すとサーバーと通信するように

--- app/App.js
+++ app/App.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, Alert, Button } from 'react-native';
+import LottieView from 'lottie-react-native';
+const io = require('socket.io-client');
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);    
+    this.state = {
+      isPress: false
+    };
+    this.socket = io("http://c37ff95a.ngrok.io", {
+      transports: ['websocket'],
+    });
+  }
+
+  componentDidMount() {        
+    this.socket.on('connect', () => {
+      this.setState({ isConnected: true });
+    });
+
+    this.socket.on('ping', data => {
+      this.setState(data);
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        { this.state.isPress && <LottieView source={require('./5390-water-animation-on-the-map.json')} autoPlay loop /> }        
+        <TouchableWithoutFeedback          
+          onPressIn={() => {
+            this.setState({
+              isPress: true
+            });
+            this.socket.emit("pressIn");
+          }}
+          onPressOut={() => {
+            this.setState({
+              isPress: false
+            });
+            this.socket.emit("pressEnd");
+          }}
+        >
+          <Text> This text is the target to be highlighted </Text>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
