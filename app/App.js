a6a0cc72bc6de1ee105d8a9cc013cc592b4bc27f
hideyasu <kutuzov1228@gmail.com>
2019-05-01 01:08:57 +0900

    アプリ起動中はスリープしないようにした

--- app/App.js
+++ app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableWithoutFeedback, Alert, Button } from 'react-native';
 import LottieView from 'lottie-react-native';
+import KeepAwake from 'react-native-keep-awake';
 const io = require('socket.io-client');
 
 export default class App extends React.Component {
@@ -25,27 +26,52 @@ export default class App extends React.Component {
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        { this.state.isPress && <LottieView source={require('./5390-water-animation-on-the-map.json')} autoPlay loop /> }        
-        <TouchableWithoutFeedback          
-          onPressIn={() => {
-            this.setState({
-              isPress: true
-            });
-            this.socket.emit("pressIn");
-          }}
-          onPressOut={() => {
-            this.setState({
-              isPress: false
-            });
-            this.socket.emit("pressEnd");
-          }}
-        >
-          <Text> This text is the target to be highlighted </Text>
-        </TouchableWithoutFeedback>
-      </View>
-    );
+    if (this.props.screenShouldBeAwake) {
+      return (
+        <View style={styles.container}>
+          { this.state.isPress && <LottieView source={require('./5390-water-animation-on-the-map.json')} autoPlay loop /> }        
+          <TouchableWithoutFeedback          
+            onPressIn={() => {
+              this.setState({
+                isPress: true
+              });
+              this.socket.emit("pressIn");
+            }}
+            onPressOut={() => {
+              this.setState({
+                isPress: false
+              });
+              this.socket.emit("pressEnd");
+            }}
+          >
+            <Text> This text is the target to be highlighted </Text>
+          </TouchableWithoutFeedback>
+          <KeepAwake />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          { this.state.isPress && <LottieView source={require('./5390-water-animation-on-the-map.json')} autoPlay loop /> }        
+          <TouchableWithoutFeedback          
+            onPressIn={() => {
+              this.setState({
+                isPress: true
+              });
+              this.socket.emit("pressIn");
+            }}
+            onPressOut={() => {
+              this.setState({
+                isPress: false
+              });
+              this.socket.emit("pressEnd");
+            }}
+          >
+            <Text> This text is the target to be highlighted </Text>
+          </TouchableWithoutFeedback>
+        </View>
+      );
+    }
   }
 }
 
